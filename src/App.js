805580e412470f7cb116eda970db06f0e1c512e6
2yb <wins.varun1912@gmail.com>
2022-08-09 16:01:41 +0530

    todo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App(){
+  return (
+    <div className="App"> 
+<h1>To do list</h1>
+<br />
+<h1>
+<input type="text" placeholder="Add a task" />
+<button> + </button>
+<ol><li>I want a pie</li></ol>
+
+</h1>
+    </div>
+  );
+}
+export default App;
